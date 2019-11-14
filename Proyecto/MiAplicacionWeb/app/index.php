fa3735a6bc2b92741eeeaa16f441272ea904d72a
Julio Tejeira <jtcesarin1998@gmail.com>
2019-11-14 01:45:04 -0500

    ajustes

--- Proyecto/MiAplicacionWeb/app/index.php
+++ Proyecto/MiAplicacionWeb/app/index.php
@@ -1,17 +1,17 @@
 <?php
-$conn = new mysqli("mysql", "root", "clave123", "mi_db");
+$conn = new mysqli("mysql", "root", "juliotupapiriko06", "Secretaria_administrativa");
 // Verifica conexión
 if ($conn->connect_error) {
 	die("La conexión falló: " . $conn->connect_error);
 }else echo "Conexión exitosa";
 
-$sql = "SELECT nombre FROM usuarios";
+$sql = "SELECT titulo FROM diplomados";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	// Datos de salida de cada registro
 	while($row = $result->fetch_assoc()) {
-		echo $row['nombre']."<br>";
+		echo $row['titulo']."<br>";
 	}
 } else {
 	echo "Ningún resultado para la tabla usuarios";
