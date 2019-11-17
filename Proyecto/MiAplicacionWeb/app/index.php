c5bbf215c612f842c0ae94c29027c47e6e6c9191
Julio Tejeira <jtcesarin1998@gmail.com>
2019-11-16 21:57:28 -0500

    ajustes a la base de datos

--- Proyecto/MiAplicacionWeb/app/index.php
+++ Proyecto/MiAplicacionWeb/app/index.php
@@ -5,13 +5,14 @@ if ($conn->connect_error) {
 	die("La conexión falló: " . $conn->connect_error);
 }else echo "Conexión exitosa";
 
-$sql = "SELECT titulo FROM diplomados";
+$sql = "SELECT titulo_obtenido, iddiplomados FROM diplomados_docentes";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	// Datos de salida de cada registro
 	while($row = $result->fetch_assoc()) {
-		echo $row['titulo']."<br>";
+		echo $row['titulo_obtenido']."<br>";
+		echo $row['iddiplomados']."<br>";
 	}
 } else {
 	echo "Ningún resultado para la tabla usuarios";
